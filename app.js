889b84b95fe6c97f5918bcbd450c09528279855f
Sumbono <sumbono@live.com>
2020-12-05 01:20:39 +0700

    add jokes refresher

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { fetchJokes } from "./fetcher.js";
+import { fetchJokes } from "./libs.js";
 import express from 'express';
 import axios from 'axios';
 import fs from 'fs';
@@ -6,20 +6,47 @@ import fs from 'fs';
 const fetching_jokes = fetchJokes();
 const app = express();
 const port = 3000;
-var jokes = [];
-const url = 'http://api.icndb.com/jokes/random/';
 
-function axiosTest(url) {
-    return axios.get(url).then(response => response.data.value.joke)
-}
-
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     fs.readFile("jokes.json", function(err, data) {  
         if (err) throw err; 
-        const users = JSON.parse(data);
-        console.log(users); // Print users
-        res.send(users);
+        const jokes = JSON.parse(data);
+        const chunked_jokes = {};
+        var count = 0;
+        for (var x in jokes) {
+            chunked_jokes[x] = jokes[x];
+            count += 1;
+            if (count == 5) {break;}
+        }
+        console.log(chunked_jokes);
+        res.send(chunked_jokes);
     });
+});
+
+app.get('/api/refresh_jokes', (req, res) => {
+    fetchJokes();
+    fs.readFile("jokes.json", function(err, data) {  
+        if (err) throw err; 
+        const jokes = JSON.parse(data);
+        const chunked_jokes = {};
+        var count = 0;
+        for (var x in jokes) {
+            chunked_jokes[x] = jokes[x];
+            count += 1;
+            if (count == 10) {break;}
+        }
+        // console.log(chunked_jokes);
+        res.send(chunked_jokes);
+    });
+});
+
+app.delete('/api/delete_all', (req, res) => {
+    const json_jokes = {};
+    fs.writeFile("jokes.json", JSON.stringify(json_jokes, null, "\t"), err => { 
+        if (err) throw err;
+        console.log("Done removing all jokes"); 
+    });
+    res.send("All jokes removed.")
 });
 
 app.listen(port, () => console.log('Listening on port: ' + port) )
